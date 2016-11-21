aba2ebf5b508e7ca1b93ab81d78e7bcbdfb7d41d
Vitor Freitas <vitorfs@gmail.com>
2016-11-21 11:39:12 +0200

    Remove cache

--- js/latest-comments.js
+++ js/latest-comments.js
@@ -3,7 +3,6 @@ $(function () {
     url: '/api/latest-comments/',
     type: 'get',
     dataType: 'json',
-    cache: false,
     success: function (data) {
       $("#latest-comments").html(data.html);
     },
