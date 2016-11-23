503dd03c7f531e7dab2add75cf4e4e3c91b9606c
Vitor Freitas <vitorfs@gmail.com>
2016-11-23 17:46:41 +0200

    Update page views script

--- js/page-views.js
+++ js/page-views.js
@@ -1,22 +1,24 @@
 $(function () {
   var urls = [];
+  var data = {};
+
   $("[data-ga-identifier]").map(function(){
     urls.push($(this).attr("data-ga-identifier"));
   });
+
+  if ($("#ga-page-views").attr("data-filter") !== "all") {
+    data['urls'] = urls;
+  }
+
   $.ajax({
     url: '/api/page-views/',
-    data: {
-      'url': urls
-    },
+    data: data,
     type: 'get',
     dataType: 'json',
     success: function (data) {
       urls.forEach(function (url) {
         $("[data-ga-identifier='" + url + "']").text(data[url]);
       });
-    },
-    error: function () {
-
     }
   });
 });
